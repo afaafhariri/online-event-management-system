6f07a3a33014280b538bfd421e5c8ab8ff7b56bc
Afaaf Hariri <haririafaaf@gmail.com>
2024-05-22 23:23:56 +0530

    No password DB

--- src/main/java/booking/DBConnection.java
+++ src/main/java/booking/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/mydatabase";
     private static final String USER = "root";
-    private static final String PASSWORD = "har&af23!SLIIT";
+    private static final String PASSWORD = "";
     
     public static Connection getConnection() {
         Connection connection = null;
